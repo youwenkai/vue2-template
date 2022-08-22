8c83254e1feaf5e0a27f71e34be22d8c893ec5b4
wkyou <wkyou@iflytek.com>
2022-08-22 16:49:31 +0800

    feat: 集成eslint prettier editorconfig

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { sum } from "./a.js";
+
+const a = 10;
+sum(1, 10);
+
+const b = "121321";
